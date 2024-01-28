86b43dc91a00cfd13950a3604d0ccf2d0b36c48b
INSANEATTIC <135711002+INSANEATTIC@users.noreply.github.com>
2024-01-28 14:39:31 +0500

    Update script.js

--- heavensfeel/js/script.js
+++ heavensfeel/js/script.js
@@ -77,24 +77,66 @@ $(document).ready(function(){
 
 if (window.innerHeight > window.innerWidth || window.innerWidth < 520){
 
+	var scrollDown = document.head.appendChild(document.createElement("style"));
+	var content = document.getElementById("content");
+
+	scrollDown.innerHTML = "#scroll-down:before {font-size: 100px;}";
+
+	content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/cover-m.jpg) center no-repeat fixed #000000";
+	content.style.backgroundSize = "cover";
+	content.style.transition = "0s";
+
 	document.getElementById("subtitle").style.width = "100%";
-	document.getElementById("subtitle").style.height = "100px";
-	document.getElementById("subtitle").style.top = "30%";
+	document.getElementById("subtitle").style.height = "30%";
+	document.getElementById("subtitle").style.top = "0";
+	document.getElementById("subtitle").style.backgroundPositionY = "100%";
 	document.getElementById("credits").style.height = "150px";
 	document.getElementById("credits").style.top = "2.5%";
 	document.getElementById("typemoon").style.width = "500px";
 	document.getElementById("typemoon").style.height = "150px";
 	document.getElementById("insaneattic").style.width = "500px";
 	document.getElementById("insaneattic").style.height = "150px";
+	document.getElementById("logo").style.gap = "0";
 	document.getElementById("logo-1").style.width = "100%";
-	document.getElementById("logo-1").style.height = "58%";
+	document.getElementById("logo-1").style.height = "56%";
 	document.getElementById("logo-1").style.backgroundPositionY = "100%";
 	document.getElementById("logo-2").style.width = "100%";
-	document.getElementById("logo-2").style.height = "42%";
+	document.getElementById("logo-2").style.height = "30%";
 	document.getElementById("logo-2").style.backgroundPositionY = "0%";
 	document.getElementById("scroll-down").style.width = "150px";
 	document.getElementById("scroll-down").style.height = "300px";
 	document.getElementById("scroll-down").style.borderRadius = "100px";
 	document.getElementById("scroll-title").style.fontSize = "60px";
+	document.getElementById("scroll-title").style.visibility = "hidden";
+	document.getElementById("scroll-title").style.opacity = "0";
+	document.getElementById("divider-1").style.bottom = "0";
+
+	window.onscroll = function (event) {
+
+		var scroll = window.pageYOffset;
+
+		if (scroll <= 20) {
+			content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/cover-m.jpg) center no-repeat fixed #000000";
+			content.style.backgroundSize = "cover";
+			content.style.transition = "0.5s ease";
+			document.getElementById("logo").style.visibility = "visible";
+			document.getElementById("logo").style.opacity = "1";
+			document.getElementById("subtitle").style.visibility = "visible";
+			document.getElementById("subtitle").style.opacity = "1";
+			document.getElementById("scroll-down").style.visibility = "visible";
+			document.getElementById("scroll-down").style.opacity = "1";;
+			document.getElementById("divider-1").style.visibility = "hidden";
+			document.getElementById("divider-1").style.opacity = "0";
+			}
+
+		else if (scroll > 20) {
+			content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/background-m.jpg) center no-repeat fixed #000000";
+			content.style.backgroundSize = "cover";
+			content.style.transition = "0.5s ease";
+			document.getElementById("divider-1").style.visibility = "visible";
+			document.getElementById("divider-1").style.opacity = "1";
+			}
+
+		}
 
 	}
