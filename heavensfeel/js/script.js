6585eb70ed87e0ca2371e175c6db39bb56c29495
INSANEATTIC <135711002+INSANEATTIC@users.noreply.github.com>
2024-03-01 08:40:12 +0500

    Update script.js

--- heavensfeel/js/script.js
+++ heavensfeel/js/script.js
@@ -20,28 +20,12 @@ window.onscroll = function (event) {
 		content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/cover.jpg) center no-repeat fixed #000000";
 		content.style.backgroundSize = "cover";
 		content.style.transition = "0.5s ease";
-		document.getElementById("logo").style.visibility = "visible";
-		document.getElementById("logo").style.opacity = "1";
-		document.getElementById("subtitle").style.visibility = "visible";
-		document.getElementById("subtitle").style.opacity = "1";
-		document.getElementById("scroll-down").style.visibility = "visible";
-		document.getElementById("scroll-down").style.opacity = "1";
-		document.getElementById("scroll-title").style.visibility = "visible";
-		document.getElementById("scroll-title").style.opacity = "1";
-		document.getElementById("divider-1").style.visibility = "hidden";
-		document.getElementById("divider-1").style.opacity = "0";
 		}
 
 	else if ((scroll >= 20 && scroll <= 600) || (scroll >= 600 && scroll <= 1000)) {
 		content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/interlude.jpg) center no-repeat fixed #000000";
 		content.style.backgroundSize = "cover";
 		content.style.transition = "0.5s ease";
-		document.getElementById("scroll-down").style.visibility = "hidden";
-		document.getElementById("scroll-down").style.opacity = "0";
-		document.getElementById("scroll-title").style.visibility = "hidden";
-		document.getElementById("scroll-title").style.opacity = "0";
-		document.getElementById("divider-1").style.visibility = "visible";
-		document.getElementById("divider-1").style.opacity = "1";
 		}
 
 	else if ((scroll >= 1000 && scroll <= 1800)) {
@@ -125,10 +109,6 @@ if (window.innerHeight > window.innerWidth) {
 	document.getElementById("scroll-title").style.display = "none";
 	document.getElementById("scroll-title").style.fontSize = "60px";
 	document.getElementById("scroll-title").style.opacity = "0";
-	document.getElementById("divider-1").style.top = "0";
-	document.getElementById("divider-2").style.top = "0";
-	document.getElementById("divider-3").style.top = "0";
-	document.getElementById("footer").style.top = "calc(100% + 2432px)";
 
 	window.onscroll = function (event) {
 
@@ -138,16 +118,12 @@ if (window.innerHeight > window.innerWidth) {
 			content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/cover-m.jpg) center no-repeat fixed #000000";
 			content.style.backgroundSize = "cover";
 			content.style.transition = "0.5s ease";
-			document.getElementById("divider-1").style.visibility = "hidden";
-			document.getElementById("divider-1").style.opacity = "0";
 			}
 
 		else if (scroll > 20) {
 			content.style.background = "url(https://raw.githubusercontent.com/INSANEATTIC/insaneattic.github.io/main/heavensfeel/assets/background-m.jpg) center no-repeat fixed #000000";
 			content.style.backgroundSize = "cover";
 			content.style.transition = "0.5s ease";
-			document.getElementById("divider-1").style.visibility = "visible";
-			document.getElementById("divider-1").style.opacity = "1";
 			}
 
 		}
